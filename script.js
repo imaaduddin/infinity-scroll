3c06fb7d19faf8f8eb7d1ebc9dd07296f36e4aaf
Imaad Uddin <imaaduddin@icloud.com>
2020-12-05 13:23:10 -0600

    added the infiny scroll feature as user is scrolling down and finished project will add dark mode feature soon

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 const imageContainer = document.getElementById("image-container");
 const loader = document.getElementById("loader");
 
+let read = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 
 // Unsplash API 
-const count = 10;
+const count = 30;
 const apiKey = "tIll1mt4gQKOGrJeS993jWui_spw-BN4UZO3wjp6xRg";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// cheack if all images were loaded 
+function imageLoaded() {
+  imagesLoaded++;
+  console.log(imagesLoaded);
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+    console.log("ready =", ready);
+  }
+}
 
 // Helper Fuction to Set Attributes on DOM Elements 
 function setAttributes(element, attributes) {
@@ -18,6 +32,9 @@ function setAttributes(element, attributes) {
 
 // Create Elements For Links & Photos, Add To DOM
 function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+  console.log("total images", totalImages);
   // Run function for each object in photosArray
   photosArray.forEach((photo) => {
     // Create <a> to link to Unsplash
@@ -33,6 +50,8 @@ function displayPhotos() {
       alt: photo.alt_description,
       title: photo.alt_description,
     });
+    // Eveent Listener, check when each is finished loading 
+    img.addEventListener("load", imageLoaded);
     // Put <img> inside <a>, then put both isnide imageConatiner Element 
     item.appendChild(img);
     imageContainer.appendChild(item);
@@ -49,6 +68,14 @@ async function getPhotos() {
     // Catch Error Here
   }
 }
+
+// Check to see if scrolling near bottom of page, Load more photos 
+window.addEventListener("scroll", () => {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+    ready = false;
+    getPhotos();
+  }
+});
 
 // On Load 
 getPhotos();
